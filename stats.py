239886151d1713460541c9352564b15ab1ef2b41
Gina Sprint <sprint@gonzaga.edu>
2021-03-29 19:58:58 -0700

    with more titles

--- stats.py
+++ stats.py
@@ -13,7 +13,7 @@ for container in client.containers.list():
     stats_dict = container.stats(stream=False)
     for key, value in stats_dict.items():
         outfile.write(str(key) + ":" + str(value) + "\n")
-    outfile.write("top: " + str(container.top()) + "\n")
+    outfile.write("top: " + str(container.top(ps_args="uxS")) + "\n")
 outfile.write("\n")
 
 # https://docker-py.readthedocs.io/en/stable/networks.html
