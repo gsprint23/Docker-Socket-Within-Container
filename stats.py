bcdc34fd72a5d4e93610ea25533be1a734fd1dea
Gina Sprint <sprint@gonzaga.edu>
2021-03-29 17:18:39 -0700

    add top()

--- stats.py
+++ stats.py
@@ -3,16 +3,20 @@ import docker # https://docker-py.readthedocs.io/en/stable/
 # need to mount volume for /var/run/docker.sock:/var/run/docker.sock
 # in docker-compose.yml to have access to the docker socket
 client = docker.from_env()
+outfile = open("temp.txt", "w")
 
 # https://docker-py.readthedocs.io/en/stable/containers.html
 for container in client.containers.list():
-    print("\n\n", container.name, container.status, container.ports)
+    outfile.write("\n" + str(container.name) + "\n")
+    outfile.write(str(container.status) + "\n")
+    outfile.write(str(container.ports) + "\n")
     stats_dict = container.stats(stream=False)
     for key, value in stats_dict.items():
-        print(key, ":", value)
-print("\n\n")
+        outfile.write(str(key) + ":" + str(value) + "\n")
+    outfile.write("top: " + str(container.top()) + "\n")
+outfile.write("\n")
 
 # https://docker-py.readthedocs.io/en/stable/networks.html
 for network in client.networks.list():
-    print("\n\n", network.name, network.containers)
-print("\n\n")
+    outfile.write("\n" + str(network.name) + " " + str(network.containers))
+outfile.close()
